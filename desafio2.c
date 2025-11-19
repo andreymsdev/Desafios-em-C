1d2e1c9c1a562a6ba9150c9a93550a9577bf12d6
Andrey de Moraes <andreym.professional@gmail.com>
2025-11-19 19:07:54 -0300

    Create desafio2.c

---
+++ desafio2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b, resultado;
+    char op;
+
+    // TODO: peça ao usuário o primeiro número
+    printf("Digite o primeiro número: ");
+    scanf("%d", &a);
+
+    // TODO: peça ao usuário o segundo número
+    printf("Digite o segundo número: ");
+    scanf("%d", &b);
+
+    // TODO: peça ao usuário a operação (+ - * /)
+    printf("Digite a operação (+ - * /): ");
+    scanf(" %c", &op);
+
+    // TODO: use switch para calcular
+    switch(op) {
+        case '+':
+            resultado = a + b;
+            break;
+        case '-':
+            resultado = a - b;
+            break;
+        case '*':
+            resultado = a * b;
+            break;
+        case '/':
+            resultado = a / b;
+            break;
+        default:
+            printf("Operação inválida!\n");
+            return 1;
+    }
+
+    // TODO: imprima o resultado
+    printf("Resultado: %d\n", resultado);
+
+    return 0;
+}
